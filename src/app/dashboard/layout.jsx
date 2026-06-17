87be5e242dc5eaf213eed81b39c5118bac45a8dd
afrid2004 <mdfaisalafrid@gmail.com>
2026-06-17 12:27:31 +0600

    added multiple layout like dashboard

---
+++ src/app/dashboard/layout.jsx
@@ -0,0 +1,44 @@
+import Link from "next/link";
+import React from "react";
+
+const layout = ({ children }) => {
+  return (
+    <div className="my-5">
+      <div className="grid grid-cols-12 gap-5 min-h-screen">
+        <div className="col-span-3">
+          <div className="border-r-2 border-gray-700 h-full overflow-y-auto flex flex-col gap-3 pr-5">
+            <Link
+              className="bg-gray-800 hover:bg-cyan-600 rounded-lg duration-75 px-4 py-2"
+              href="/dashboard"
+            >
+              Home
+            </Link>
+            <Link
+              className="bg-gray-800 hover:bg-cyan-600 rounded-lg duration-75 px-4 py-2"
+              href="/dashboard/add-story"
+            >
+              Add Story
+            </Link>
+            <Link
+              className="bg-gray-800 hover:bg-cyan-600 rounded-lg duration-75 px-4 py-2"
+              href="/dashboard/my-profile"
+            >
+              My Profile
+            </Link>
+            <Link
+              className="bg-gray-800 hover:bg-cyan-600 rounded-lg duration-75 px-4 py-2"
+              href="/dashboard/settings"
+            >
+              Settings
+            </Link>
+          </div>
+        </div>
+        <div className="col-span-9">
+          <div>{children}</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default layout;
